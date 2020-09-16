5e73589141a97f28d2dcb62ea9b0fd237bcb20a0
nokranger <anusorn.thavornporn@gmail.com>
2020-09-16 11:30:40 +0700

    update

--- api/routes/customer.js
+++ api/routes/customer.js
@@ -5,73 +5,108 @@ const bodyParser = require('body-parser')
 route.use(bodyParser.json())
 
 route.post('/add', (req, res) => {
-  console.log('code', req.body.code)
+  console.log('code', req.body.cus_code)
   console.log('test')
   connection.getConnection((err, con) => {
     if (err) throw err;
     var sql = "INSERT INTO customer (cus_code, cus_name, lat, lon, route_id, available, create_at, update_at, delete_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
     var sql2 = 'SELECT * FROM customer'
-    var value = [req.body.cus_code, req.body.cus_name, req.body.lat, '', req.body.lon, req.body.available, req.body.create_at, '', '']
+    var value = [req.body.cus_code, req.body.cus_name, req.body.lat, req.body.lon, '', req.body.available, req.body.create_at, '', '']
     connection.query(sql, value, (err, result, fields) => {
-      // connection.query(sql2, (err, result, fields) => {
-      //   console.log('query2')
-      //   console.log(result)
-      //   if (err) {
-      //     res.status(404).json({
-      //       err: err
-      //     })
-      //   }
-      //   if (result.length > 0) {
-      //     res.status(200).json({
-      //       result: result
-      //     })
-      //   }
-      // })
+      connection.query(sql2, (err, result, fields) => {
+        console.log('query2')
+        console.log(result)
+        if (err) {
+          res.status(404).json({
+            err: err
+          })
+        }
+        if (result.length > 0) {
+          res.status(200).json({
+            result: result
+          })
+        }
+      })
       // console.log('bbbbbbb', typeof(result));
       console.log('insert done')
     });
-    
     con.release()
   });
 })
 
-route.post('/login', (req, res) => {
-  console.log('id', req.body.test)
-  console.log('password', req.body.password)
+route.get('/get', (req, res) => {
   connection.getConnection((err, con) => {
     if (err) throw err;
-    var sql = 'SELECT user_id, password FROM member WHERE user_id = ? AND password = ?'
-    var value = [req.body.user_id, req.body.password]
-    connection.query(sql, value, (err, result, fields) => {
-      if (err) throw err;
-      // console.log(result);
-      if (result.length > 0) {
-        // console.log(result[0].user_id)
-        // console.log(result[0].permission)
-        const token = jwt.sign(
-          {
-          sub: result[0].user_id,
-          iat: new Date().getTime(), // issue at time,
-          role: 1,
-          loginSuccessfull: true,
-          },process.env.JWT_KEY,
-          {
-            expiresIn: '10m'
+    var sql = 'SELECT * FROM customer'
+      connection.query(sql, (err, result, fields) => {
+        console.log('query2')
+        console.log(result)
+        if (err) {
+          res.status(404).json({
+            err: err
           })
-        console.log('success')
-          // res.status(200).json(jwt.encode(payload, SECRET))
-        res.status(200).json(token)
-      } else {
-        console.log('unsuccess')
-        res.status(404).json({
-          message: 'NOT FOUND'
-        })
-      }
-      con.release()
-    });
+        }
+        if (result.length > 0) {
+          res.status(200).json({
+            result: result
+          })
+        }
+      })
+      // console.log('bbbbbbb', typeof(result));
+      console.log('insert done')
+    con.release()
   });
-  console.log('done selected')
 })
 
+route.post('/getdetail', (req, res) => {
+  connection.getConnection((err, con) => {
+    if (err) throw err;
+    var sql = 'SELECT * FROM `customer` WHERE cus_code = ?'
+    var value = [req.body.name]
+      connection.query(sql, value, (err, result, fields) => {
+        if (err) {
+          res.status(404).json({
+            err: err
+          })
+        }
+        if (result.length > 0) {
+          res.status(200).json({
+            result: result
+          })
+        }
+      })
+      // console.log('bbbbbbb', typeof(result));
+      console.log('get detail')
+    con.release()
+  });
+})
+
+route.patch('/updatedetail', (req, res) => {
+  connection.getConnection((err, con) => {
+    if (err) throw err;
+    var sql = 'UPDATE customer SET route_id = ?, update_at = ? WHERE cus_code = ?'
+    var sql2 = 'SELECT * FROM `customer` WHERE cus_code = ?'
+    var value = [req.body.route_id, req.body.update_at, req.body.cus_code]
+    var value2 = [req.body.cus_code]
+    console.log(value)
+      connection.query(sql, value, (err, result, fields) => {
+        connection.query(sql2, value2, (err, result, fields) => {
+          if (err) {
+            res.status(404).json({
+              err: err
+            })
+          }
+          if (result.length > 0) {
+            res.status(200).json({
+              result: result
+            })
+          }
+        })
+      })
+      // console.log('bbbbbbb', typeof(result));
+      console.log('update detail')
+    con.release()
+  });
+})
 
 module.exports = route
